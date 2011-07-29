53ae4bb7f8cf208008ec09b80e3ba9c25206e935
unknown <glenax@.gov.dnvr>
2011-07-28 22:23:16 -0600

    Added support for configurable format type actions.

---
+++ CkanDotNet.Web/Models/Settings/ResourceSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CkanDotNet.Web.Models.Settings
+{
+    public class ResourceSettings
+    {
+        Dictionary<string,ResourceType> types = new Dictionary<string,ResourceType>();
+
+        public Dictionary<string,ResourceType> Types
+        {
+            get
+            {
+                return types;
+            }
+        }
+    }
+}
