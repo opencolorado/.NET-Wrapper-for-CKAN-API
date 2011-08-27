6c7d8350c9e64e14427bc5080ba99c0063aef078
Allan Glen <allan@allanglen.com>
2011-08-27 14:48:52 -0600

    Working on handling CKAN exceptions.

---
+++ CkanDotNet.Api/Exceptions/CkanException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CkanDotNet.Api.Exceptions
+{
+    public class CkanException : Exception
+    {
+        public CkanException(string message)
+            : base(message)
+        {
+        }
+
+        public CkanException(string message, Exception ex)
+            : base(message, ex)
+        {
+
+        }
+    }
+}
---
+++ CkanDotNet.Api/Exceptions/CkanTimeoutException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CkanDotNet.Api.Exceptions
+{
+    public class CkanTimeoutException : CkanException
+    {
+        public CkanTimeoutException(string message)
+            : base(message)
+        {
+        }
+
+        public CkanTimeoutException(string message, Exception ex)
+            : base(message, ex)
+        {
+
+        }
+    }
+}
