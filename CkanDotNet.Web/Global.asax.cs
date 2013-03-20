30694fb01c26d942004358c890ba00de8598da0d
Allan Glen <allan@allanglen.com>
2013-03-20 16:33:15 -0600

    Add favicon.ico to routes to be ignored

--- CkanDotNet.Web/Global.asax.cs
+++ CkanDotNet.Web/Global.asax.cs
@@ -28,6 +28,7 @@ namespace CkanDotNet.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("favicon.ico");
 
             routes.MapRoute(
                 "Error",
